8b4a51b2fb502bb264920ebca4a7242a087a5ae8
amrahimi-github <amirmasoudrahimiofficial@gmail.com>
2021-12-25 20:46:03 +0330

    Add Config to WebApiConfig.cs

--- MessagingWebApplication/App_Start/WebApiConfig.cs
+++ MessagingWebApplication/App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace MessagingWebApplication
 {
@@ -9,8 +11,14 @@ namespace MessagingWebApplication
     {
         public static void Register(HttpConfiguration config)
         {
+
             // Web API configuration and services
-               config.Filters.Add(new AuthorizeAttribute());
+            // Configure Web API to use only bearer token authentication.
+            //config.SuppressDefaultHostAuthentication();
+            //config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            // Web API configuration and services
+            config.Filters.Add(new AuthorizeAttribute());
             // Web API routes
             config.MapHttpAttributeRoutes();
 
@@ -19,6 +27,12 @@ namespace MessagingWebApplication
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // Return Json Instead of Xml as Response Format
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            // Allowed Request Coming from any Domain
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
         }
     }
 }
